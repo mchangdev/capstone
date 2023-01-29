865deb20438f34fa37301db067300cd0383c1743
mchangdev <ming.chang.college@gmail.com>
2023-01-29 11:39:05 -0500

    Add config.js in config folder

---
+++ config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    database:{
+        dbName:'capstone',
+        host:'192.168.64.2', // ip address of database
+        port:3306,
+        user:'root',
+        password:'123456',
+    },
+}
